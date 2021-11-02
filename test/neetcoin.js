275ced761bedfd5d476a96727e6ac9b180831d39
daseinsucks <alexstepov97@gmail.com>
2021-11-02 21:34:07 +0300

    Added tests

--- test/neetcoin.js
+++ test/neetcoin.js
@@ -2,27 +2,41 @@ const Neetcoin = artifacts.require('Neetcoin');
 
 contract('Neetcoin', accounts => {
     let token;
+    let coin = '20';
     const admin = accounts[0];
     const user = accounts[1];
     before(async () => {
         token = await Neetcoin.deployed();
     });
 
-    it('should mint 20 tokens to admin account', async () => {
-        let adminBalance = await token.mint(admin, 20)
-        let requiredBalance = 20
-        assert.equal(adminBalance, requiredBalance, "minting to admin completed successfully")
+    it('should mint 20 tokens to admin', async () => {
+        let tokensToMint = web3.utils.toWei(web3.utils.toBN(coin));
+        const transaction = await token.mint(admin, tokensToMint);
+        let adminTokenBalanceAfter = await token.balanceOf(admin);
+        assert.equal(adminTokenBalanceAfter.toString(), tokensToMint.toString(), 'admins token balance after mint');
     });
+  
 
     it('should not mint any tokens to user account', async () => {
-        let userBalance = await token.mint(user, 20)
-        assert.equal(userBalance, 0, "minting from user account failed")
+        let tokensToMint = web3.utils.toWei(web3.utils.toBN(coin));
+        try {
+            await token.MintERC20(user, tokensToMint, { from: user });
+        } catch (e) {
+            assert(e.message, 'error message must contain revert Ownable');
+        }
     });
 
-    it('should send 25 tokens from admin to user', async () => {
-        let adminBalance = await token.mint(admin, 5)
-        let userBalance = await token.send(admin, user, 25)
-        assert.equal(adminBalance, 0, "tokens withdrawal from admin completed successfully")
-        assert.equal(userBalance, 25, "transaction from admin recieved successfully")
+
+    it('should send 20 tokens from admin to user', async () => {
+        let tokensToMint = web3.utils.toWei(web3.utils.toBN(coin));
+        let tokensToSend = web3.utils.toWei(web3.utils.toBN(coin));
+        let adminTokenBalanceBefore = await token.balanceOf(admin);
+        const transaction = await token.mint(admin, tokensToMint);
+        const transferToken = await token.send(admin, user, tokensToSend);
+        let adminTokenBalanceAfter = await token.balanceOf(admin);
+        let userTokenBalanceAfter = await token.balanceOf(user);
+        assert.equal(adminTokenBalanceBefore.toString(), adminTokenBalanceAfter.toString(), 'withdrawal from admin account done successfully');
+        assert.equal(userTokenBalanceAfter.toString(), tokensToSend.toString(), 'user recieved tokens successfully');
+       
     });
 });
