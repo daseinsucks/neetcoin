71aa7bdb7ce239eaba6d85de332adc08e0a70ad3
daseinsucks <alexstepov97@gmail.com>
2021-11-29 23:20:13 +0300

    testing branch

---
+++ contracts/BranchExample.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.9;
+
+import "./Neetcoin.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract ntcSell is Ownable {
+    Neetcoin _neetcoin;
+
+    constructor(address token_address) {
+        _neetcoin = Neetcoin(token_address);
+    }
+
+
+fallback() external payable {
+        sellNtc();
+      }
+
+    function sellNtc() payable public {
+      address owner = owner();
+      address whoToSend = msg.sender;
+      uint256 ntcToMint = msg.value;
+      _neetcoin.mint(whoToSend, ntcToMint);
+      owner.call{value: ntcToMint}('');
+    }
+}
